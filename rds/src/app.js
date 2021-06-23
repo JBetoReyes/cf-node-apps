3c882647f98bf7e5fdbe89c83067327fb5e08e8a
Beto <josereyes55@hotmail.com>
2021-06-22 21:28:16 -0500

    :zap: handles error signals in a better way

--- rds/src/app.js
+++ rds/src/app.js
@@ -10,3 +10,16 @@ routerManager.registerRoutes(app);
 app.listen(appPort, () => {
   debug(`App listening on port ${appPort}`)
 });
+
+//do something when app is closing
+process.on('exit', () => process.exit());
+
+//catches ctrl+c event
+process.on('SIGINT', () => process.exit());
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', () => process.exit());
+process.on('SIGUSR2', () => process.exit());
+
+//catches uncaught exceptions
+process.on('uncaughtException', () => process.exit());
